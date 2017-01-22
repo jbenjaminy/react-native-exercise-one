37315196daa1535833a94b6a76afead0f7889ff9
jbenjaminy <young.benjamin@gmail.com>
2017-01-22 17:03:28 -0600

    adds imports and comments to index.ios.js

--- index.ios.js
+++ index.ios.js
@@ -3,7 +3,16 @@
     // components are anything to be rendered on the screen.
 
 // Step 1: Import a library to help create the component.
+// Import Statements:
+    // Part of ES6 -- no file gets global access.
+    // Only by importing files or modules can we access code outside of file.
+// React Library:
+    // Knows how a component should behave.
+    // Knows how to take a bunch of components and make them work together.
 import React from 'react';
+// React Native Library:
+    // Knows how to take the output from a component and place it on the screen.
+    // Provides default core components (image, text)
 import ReactNative from 'react-native';
 
 // Step 2: Create a component.
