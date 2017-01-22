79c9d17ae0cc19d9d2e1d4693e89260fe648fe31
jbenjaminy <young.benjamin@gmail.com>
2017-01-22 16:58:01 -0600

    updates index.js

---
+++ index.ios.js
@@ -0,0 +1,11 @@
+// index.ios.js
+// apps we build are composed of a collection of many components
+    // components are anything to be rendered on the screen.
+
+// Step 1: Import a library to help create the component.
+import React from 'react';
+import ReactNative from 'react-native';
+
+// Step 2: Create a component.
+
+// Step 3: Render it to the device.
