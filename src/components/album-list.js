79b790122c6d8617dbe6840ab4dcb9a9fb359094
jbenjaminy <young.benjamin@gmail.com>
2017-01-25 00:35:11 -0600

    adds initial state declaration to album-list component

--- src/components/album-list.js
+++ src/components/album-list.js
@@ -24,6 +24,17 @@ import axios from 'axios';
     // notification of that and use it to fetch data, for example.
 
 class AlbumList extends Component {
+    // STEP #1
+    // Add class-level property to set initial state.
+    // an earlier iteration of this state declaration was 'getInitialState'
+    // Whenever a componenet is created, it will now have a property called
+    // this.state, set to an empty object, which represents our initial, base
+    // state.
+        // Have an empty list of albums when component first renders -- it will
+        // have access to a variable that we can make reference to inside of our
+        // render method, that we can refer to as this.state.albums and that's
+        // going to return an empty array.
+    state = { albums: [] };
     // anytime component is about to be rendered to our device, this method
     // will automatically be called for us.
     // Perfect location for initiating a data load in your app.
@@ -32,6 +43,19 @@ class AlbumList extends Component {
         // Making an http request is inherently asynchronous, so this returns
         // a promise back to us. Function passed to .then() will be called
         // once the http request completes.
+        // When http request completes it gives us back a JSON object.
+        // http request takes some time to complete -- before we get back our
+        // data, the component tries to render. We have an order of operations
+        // problem and must tell our component to re-render when we have this
+        // data.
+        // Must enable component-level state, with the goal of rerendering the
+        // component once new data is made available.
+        // Three steps to enabling component-level state:
+            // 1) Set up default or 'initial' state
+            // 2) Once we fetch some data, tell our component to re-render.
+            // 3) Make sure that our component makes use of the state (by
+            // tweaking render method).
+        // 'State' is built in system in React.
         axios.get('https://rallycoding.herokuapp.com/api/music_albums')
             .then(response => console.log(response));
     }
