9d058cd1c66d2b4f4d4f9ae056362883ebb62e94
jbenjaminy <young.benjamin@gmail.com>
2017-01-24 22:00:48 -0600

    adds initial styling for header component

--- src/components/header.js
+++ src/components/header.js
@@ -3,8 +3,20 @@ import { Text } from 'react-native';
 
 // Always name component and file the same.
 const Header = () => {
-    return <Text>Albums!</Text>;
+    // Use destructuring to reference our style.
+    const { textStyle } = styles;
+    // Use styles as props
+    return <Text style={textStyle}>Albums!</Text>;
 };
 
+// Styling consists of a JavaScript object that contains certain properties
+// for specifying visual preferences. No CSS (so no CSS selectors), even
+// though a lot of it looks like CSS rules. Use camelCase instead of dashed
+// separation of words for multi-word styling.
+const styles = {
+    textStyle: {
+        fontSize: 20
+    }
+};
 // Make child componenet available to other parts of the app.
 export default Header;
