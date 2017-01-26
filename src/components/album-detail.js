c3b964c8f22203aa8efea1f4e5a866b569de9b8b
jbenjaminy <young.benjamin@gmail.com>
2017-01-25 23:33:29 -0600

    Refactors AlbumDetail component and creates card-container.js

--- src/components/album-detail.js
+++ src/components/album-detail.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
-const AlbumDetail = () => {
-
-};
+const AlbumDetail = (props) => (
+        <View>
+            <Text>{props.album.title}</Text>
+        </View>
+);
 
 export default AlbumDetail;
---
+++ src/components/card-container.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { View } from 'react-native';
+
+const CardContainer = () => {
+    return (
+        <View></View>
+    );
+};
+
+export default CardContainer;
