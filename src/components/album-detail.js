8317974475b651f1c928db2fa85f0f9b7d851833
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 01:12:14 -0600

    adds image components to album-detail.js and updates styling

--- src/components/album-detail.js
+++ src/components/album-detail.js
@@ -1,22 +1,47 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Image, Text } from 'react-native';
 import CardContainer from './card-container';
 import CardSection from './card-section';
-
-const AlbumDetail = (props) => (
+// Destructure the props, since we are only referencing the album object.
+// Two options:
+    // const AlbumDetail = ({ album: { thumbnail_image, title, artist } }) => (
+    // or as seen in line 10
+const AlbumDetail = ({ album }) => {
+    const { thumbnail_image, title, artist } = album;
+    return (
         <CardContainer>
             <CardSection>
-                <View></View>
-                <View style={styles.headerTextStyle}>
-                    <Text>{props.album.title}</Text>
-                    <Text>{props.album.artist}</Text>
+                <View>
+                    {
+                    // Image is another primitive component available through
+                    // the react-native library.
+                        // Must pass this a 'source' prop, and the prop must
+                        // be an object with a URI property, that is a link
+                        // to an image.
+                        // Also must add styling to the image, specifying
+                        // height and width, otherwise it will not appear to
+                        // render.
+                    }
+                    <Image
+                        style={styles.imageStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={styles.textStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
                 </View>
             </CardSection>
         </CardContainer>
-);
+    );
+};
 
 const styles = {
-    headerTextStyle: {
+    imageStyle: {
+        height: 50,
+        width: 50
+    },
+    textStyle: {
     // layout styling
         flexDirection: 'column',
         justifyContent: 'space-around'
