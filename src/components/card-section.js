34d503bb408de416d23ccffcaedc3cffeb7bf113
jbenjaminy <young.benjamin@gmail.com>
2017-01-26 00:23:07 -0600

    adds styling to card-section.js

--- src/components/card-section.js
+++ src/components/card-section.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const CardSection = () => {
-    return (
-        <View></View>
+const CardSection = () => (
+        <View style={styles.viewStyle}>
+        </View>
     );
+
+const styles = {
+    viewStyle: {
+    // borders & coloring
+        borderBottomWidth: 1,
+        borderColor: '#ddd',
+        backgroundColor: '#fff',
+    // spacing & orientation
+        padding: 5,
+        justifyContent: 'flex-start',
+        flexDirection: 'row',
+        position: 'relative'
+
+
+    }
 };
 
 export default CardSection;
