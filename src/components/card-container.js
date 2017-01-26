6aacf585e69a9ff9d3b8916e39ad35d6d942c81e
jbenjaminy <young.benjamin@gmail.com>
2017-01-25 23:42:12 -0600

    adds styling to card-container.js

--- src/components/card-container.js
+++ src/components/card-container.js
@@ -3,8 +3,30 @@ import { View } from 'react-native';
 
 const CardContainer = () => {
     return (
-        <View></View>
+        <View style={styles.containerStyle}>
+        </View>
     );
+};
+
+const styles = {
+    viewStyle: {
+    // border styling
+        borderWidth: 1,
+        borderRadius: 2,
+        borderColor: '#ddd',
+        borderBottomWidth: 0,
+    // shadow styling
+        shadowColor: '#000',
+        shadowOffset: { width: 0, height: 2 },
+        shadowOpacity: 0.1,
+        shadowRadius: 2,
+    // margin styling
+        marginLeft: 5,
+        marginRight: 5,
+        marginTop: 10,
+    // other styling
+        elevation: 1
+    }
 };
 
 export default CardContainer;
